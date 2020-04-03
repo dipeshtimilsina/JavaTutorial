f516be6e892f2b05a5c567e71abb7d1360a82671
dipeshtech <dipesh.nepali@gmail.com>
2020-04-03 22:59:47 +0545

    Day 7 Commit

---
+++ JavaTutorial/src/Polymorphism.java
@@ -0,0 +1,11 @@
+
+public class Polymorphism {
+	public static void main(String[] args) {
+		PlantPoly plant1= new PlantPoly();
+		Tree tree= new Tree();
+		
+		PlantPoly plant2 = tree;
+		
+	}
+
+}
