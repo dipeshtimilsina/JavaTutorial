fd170b2f51dd24c133cff238b407e71e84cd216b
dipeshtech <dipesh.nepali@gmail.com>
2020-04-04 23:43:56 +0545

    Day 8 Commit

--- JavaTutorial/src/EncapsulationAPIDocs.java
+++ JavaTutorial/src/EncapsulationAPIDocs.java
@@ -1,3 +1,30 @@
+//https://docs.oracle.com/javase/7/docs/api/java/lang/String.html
+
+
+class EncapPlany{
+	private String name;
+	
+	public static final int ID=7;
+	
+	public String getData() {
+		String data= "some stuff"+ calculateGrowthForecast();
+		return data; 
+	}
+	
+	private int calculateGrowthForecast() {
+		return 9;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
 
 public class EncapsulationAPIDocs {
 
