0584a46442a4c39b163801cd49aaed796bd461e4
dipeshtech <dipesh.nepali@gmail.com>
2020-04-04 23:32:22 +0545

    Day 8 Commit

--- JavaTutorial/src/Polymorphism.java
+++ JavaTutorial/src/Polymorphism.java
@@ -1,4 +1,4 @@
-
+//See this part again :) 
 public class Polymorphism {
 	public static void main(String[] args) {
 		PlantPoly plant1= new PlantPoly();
@@ -6,6 +6,15 @@ public class Polymorphism {
 		
 		PlantPoly plant2 = tree;
 		
+		plant2.grow();
+		plant1.grow();
+		tree.shedLeaves();
+//		plant2.shedLeaves(); //there's no shedLeaves on PlantPoly to use Polymorphism 
+		doGrow(tree);
+	}
+	
+	public static void doGrow(PlantPoly plant) {
+		plant.grow();
+	}
 	}
 
-}
--- JavaTutorial/src/Tree.java
+++ JavaTutorial/src/Tree.java
@@ -4,4 +4,8 @@ public class Tree extends PlantPoly {
 	public void grow() {
 		System.out.println("Tree is growing");
 	}
+	
+	public void shedLeaves() {
+		System.out.println("Leaves sheeding.");
+	}
 }
